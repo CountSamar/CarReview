6952b892a31fa9b592a0acb9dfd9523b34df050e
CountSamar <trentjohnbrown@gmail.com>
2023-09-21 13:19:45 -0500

    Added Sign up component

---
+++ src/client/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+const Profile = () => {
+    return 
+    
+    ( 
+        <>
+        
+        </>
+     );
+}
+ 
+export default Profile
+
