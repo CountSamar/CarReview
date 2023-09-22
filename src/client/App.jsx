a598864ed247525a0ee635f9be7c1835c9b0380d
shawntaebatiste <shawntae.batiste@aol.com>
2023-09-21 20:12:00 -0500

    added components and set up routing

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,17 +1,35 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
+import { Routes, Route } from 'react-router-dom'
 import Login from './components/Login';
+import Profile from './components/Profile';
+import Review from './components/Review';
+import SignUp from './components/SignUp';
+import NavBar from './components/NavBar';
+import WriteReview from './components/WriteReview';
+import Logout from './components/Logout';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
+    <>
+    <NavBar />
     <div className='App'>
-        <h1>Boilerplate</h1>
-        <img id='comp-img' src='./computer.png'></img>
-        <p>Replace the starter code in this template with something cool</p>
-        <Login />
+        <h1>Capstone Project</h1>
+        <img></img>
+        <p></p>
+
+        <Routes>
+          <Route path='/' element={<Profile />} />
+          <Route path='/reviews' element={<Review />} />
+          <Route path='/review/new' element={<WriteReview />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/logout' element={<Logout />} />
+        </Routes>
     </div>
+    </>
   );
 }
 
---
+++ src/client/components/Review.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const Review = () => {
+     
+    
+   return ( 
+        <>
+        <h1>Review</h1>
+        </>
+     );
+}
+ 
+export default Review;
