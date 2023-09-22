fd9837cb4171070e697b3f981534d0ee92c5091e
CountSamar <trentjohnbrown@gmail.com>
2023-09-21 20:14:15 -0500

    Added routes to components

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,23 +1,67 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { useState, useEffect } from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
 import Login from './components/Login';
+import Home from './components/Home';
 import Profile from './components/Profile';
 import Review from './components/Review';
 import SignUp from './components/SignUp';
 import NavBar from './components/NavBar';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 
-function App() {
-
+export default function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
 
   return (
-    <div className='App'>
-        <h1>Boilerplate</h1>
-        <img id='comp-img' src='./computer.png'></img>
-        <p>Replace the starter code in this template with something cool</p>
-        <Login />
-    </div>
+    <>
+    <h1>Our Project</h1>
+    <NavBar />
+    <Login />
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route
+        path="/Profile"
+        element={
+          <Profile 
+            username={username}
+            />
+        }
+      />
+      <Route 
+        path="/Review"
+        element={<Review />}/>  
+      <Route
+        path='/Login'
+        element={
+          <Login 
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            />
+          }
+        />
+
+      <Route 
+        path='/register'
+        element={
+          <SignUp 
+            email={email}
+            setEmail={setEmail}
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+            />
+          }
+      />
+    </Routes>
+   
+   </>
   );
 }
 
-export default App;
+
