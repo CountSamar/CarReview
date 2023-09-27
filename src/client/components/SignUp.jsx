ab40f230e286c8e47cdfca7e71f99e0e65b735be
CJ-Talley <132952516+CJ-Talley@users.noreply.github.com>
2023-09-26 20:27:27 -0400

    Update SignUp.jsx
    SingUp page for new users or users making an account. If any corrections needed or errors found, please let me know!

--- src/client/components/SignUp.jsx
+++ src/client/components/SignUp.jsx
@@ -1,13 +1,99 @@
-import React, { useState, useEffect } from 'react';
+// SignUp.jsx
+import React, { useState } from 'react';
 
-const SignUp = () => {
-    return 
-    
-    (
-    <>
-    
-    </>  
-    );
+function SignUp() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //
+    console.log('SignUp Form has been submitted:', formData);
+
+    // reset the form after account has been made
+    setFormData({
+      firstName: '',
+      lastName: '',
+      username: '',
+      email: '',
+      password: '',
+    });
+  };
+
+  return (
+    <div>
+      <h2>Create User</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Create User</button>
+      </form>
+    </div>
+  );
 }
- 
+
 export default SignUp;
