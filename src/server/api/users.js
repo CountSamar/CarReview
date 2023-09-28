b188da1cb9ccc379b410b96ab3ca20d4fb19102d
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-09-28 14:02:59 -0500

    Updates to cilent.js

--- src/server/api/users.js
+++ src/server/api/users.js
@@ -1,37 +1,34 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { createUser, getUserByEmail, getUser } = require('./db/index.js'); 
+
 const usersRouter = express.Router();
 
-const {
-    createUser,
-    getUser,
-    getUserByEmail
-} = require('../db');
-
-const jwt = require('jsonwebtoken')
-
-usersRouter.get('/', async( req, res, next) => {
+usersRouter.get('/', async (req, res, next) => {
     try {
         const users = await getAllUsers();
-
         res.send({
             users
         });
-    } catch ({name, message}) {
-        next({name, message})
+    } catch ({ name, message }) {
+        next({ name, message });
     }
 });
 
-usersRouter.post('/login', async(req, res, next) => {
+usersRouter.post('/login', async (req, res, next) => {
     const { email, password } = req.body;
-    if(!email || !password) {
-        next({
+
+    if (!email || !password) {
+        return next({
             name: 'MissingCredentialsError',
             message: 'Please supply both an email and password'
         });
     }
+
     try {
-        const user = await getUser({email, password});
-        if(user) {
+        const user = await getUserByEmail(email);
+        if (user && await bcrypt.compare(password, user.password_hash)) {
             const token = jwt.sign({
                 id: user.id,
                 email
@@ -43,35 +40,35 @@ usersRouter.post('/login', async(req, res, next) => {
                 message: 'Login successful!',
                 token
             });
-        }
-        else {
+        } else {
             next({
                 name: 'IncorrectCredentialsError',
                 message: 'Username or password is incorrect'
             });
         }
-    } catch(err) {
-        next(err);
+    } catch (error) {
+        next(error);
     }
 });
 
-usersRouter.post('/register', async(req, res, next) => {
+usersRouter.post('/register', async (req, res, next) => {
     const { name, email, password } = req.body;
 
     try {
         const _user = await getUserByEmail(email);
 
-        if(_user) {
-            next({
+        if (_user) {
+            return next({
                 name: 'UserExistsError',
                 message: 'A user with that email already exists'
             });
         }
 
+        const hashedPassword = await bcrypt.hash(password, 10);
         const user = await createUser({
             name,
             email,
-            password
+            password: hashedPassword
         });
 
         const token = jwt.sign({
@@ -85,9 +82,9 @@ usersRouter.post('/register', async(req, res, next) => {
             message: 'Sign up successful!',
             token
         });
-    } catch({name, message}) {
-        next({name, message})
+    } catch ({ name, message }) {
+        next({ name, message });
     }
-})
+});
 
 module.exports = usersRouter;
--- src/server/db/client.js
+++ src/server/db/client.js
@@ -1,9 +1,22 @@
-const { Client } = require('pg');
-const connectionString = process.env.DATABASE_URL || 'http://localhost:5432/Capstone';
+const { Pool } = require('pg');
 
-const db = new Client({
-    connectionString,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+const pool = new Pool({
+  host: 'dpg-ck9n8m6gtj9c73baorng-a', 
+  port: 5432, 
+  database: 'carreview', 
+  user: 'carreview_user',
+  password: '6n8KVVfZQlocyNMNRppBGRnCJzUf211i',  
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
 });
 
-module.exports = db;
+pool.connect()
+  .then(() => {
+    console.log("Successfully connected to the database!");
+    pool.end();
+  })
+  .catch(err => {
+    console.error("Error connecting to the database:", err.stack);
+    pool.end();
+  });
+
+module.exports = pool;
