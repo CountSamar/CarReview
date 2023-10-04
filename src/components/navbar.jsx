0ee60b40b4ed2fa852faa0c7f5a26810a913acfb
Brian Sanchez <briansanchez@Brians-MacBook-Pro.local>
2023-10-03 20:01:58 -0500

    code for comment section

---
+++ src/components/navbar.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+import { Link } from "react-router-dom"
+
+
+export default function NavBar() {
+
+    return (
+        <div className='contain outline'>
+
+            <div>
+                <Link to="/register" className='link-styles2'>Register</Link>
+                <Link to='/login' className='link-styles2'>Login</Link>
+            </div>
+            <div>
+                <Link to='/Profile' className='link-styles2'>Profile</Link>
+                <Link to='/logout' className='link-styles2'>Logout</Link>
+            </div>
+
+                <h1 className='title'></h1>
+                    <div className='links'>
+                        
+                        <Link to='/' className='link-styles'>Home</Link>
+                        <Link to="/posts/new" className='link-styles'>Add A Post</Link>
+                        <Link to="/posts" className='link-styles'>See All Post</Link>
+                        
+                    </div>
+        </div>
+    )
+}
