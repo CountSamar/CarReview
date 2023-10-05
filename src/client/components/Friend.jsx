8caef09365234b52cfe314973a37b3eff1746f7e
CountSamar <trentjohnbrown@gmail.com>
2023-10-04 19:09:48 -0500

    No change

--- src/client/components/Friend.jsx
+++ src/client/components/Friend.jsx
@@ -5,7 +5,7 @@ export default function Friend() {
     useEffect(() => {
         async function fetchFriends() {
           try {
-            const response = await fetch('http://localhost:3001/api/friends/1');
+            const response = await fetch('http://localhost:3000/api/friends/1');
             const result = await response.json();
             console.log("result", result.friends) ;
             setFriends(result.friends);
