bd98cc735e9a091f3649351cd20f308446d3e613
CountSamar <trentjohnbrown@gmail.com>
2023-10-04 19:11:34 -0500

    added websocket

--- src/client/components/SendMessage.jsx
+++ src/client/components/SendMessage.jsx
@@ -6,7 +6,7 @@ const SendMessage = () => {
     const [message, setMessage] = useState('')
 
   useEffect(() => {
-    const newSocket = new WebSocket('ws.//localhost:3001')
+    const newSocket = new WebSocket('ws.//localhost:3000')
     newSocket.onopen = function() {
         setSocket(newSocket)
     }
