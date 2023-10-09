f28acebf1dacec47933c82af7993bbdaaa9b69de
CountSamar <trentjohnbrown@gmail.com>
2023-10-09 13:08:02 -0500

    building a search bar

--- src/client/components/SearchBar.jsx
+++ src/client/components/SearchBar.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
+import Review from './Review'
 
-const SearchBar = () => {
-    const [searchTerm, setSearchTerm] = useState('')
-    const handleSearch
-    return ( 
-    
-    <>
+const SearchBar = ({ Review }) => {
+  const [searchTerm, setSearchTerm] = useState('');
 
-    </> 
-    
- );
-}
- 
+  const handleInputChange = (e) => {
+   setSearchTerm(e.target.value);
+    };
+    const handleSubmit = (e) => {
+        const resultsArray = Review.filter(review => review.title.includes(e.target.value) || review.body.includes(e.target.value))
+        console.log('Search term:', searchTerm)
+    }
+  return (
+    <form onSubmit={handleSubmit}>
+    <input
+      type="text"
+      placeholder="Search reviews..."
+      value={searchTerm}
+      onChange={handleInputChange}
+    />
+    <button type="submit" className="search_button">
+      Search
+    </button>
+  </form>
+  );
+};
+
 export default SearchBar;
