8125d5133ab7e0434a88946aaec1630618813b3d
CountSamar <trentjohnbrown@gmail.com>
2023-10-09 13:07:53 -0500

    working on Home

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,10 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import SearchBar from './SearchBar';
+import Review from './Review'
 
-const Home = () => {
+const Home = ({ Review }) => {
+    const ReviewsList = () => {
+        const [filteredReviews, setFilteredReviews] = useState(reviewsData);
+      
+        const handleSearch = (searchTerm) => {
+          const filtered = Review.filter((review) =>
+            review.description.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+          setFilteredReviews(filtered);
+        };
+    }
+
     return ( 
     
     <>
-    
+    <div>
+      <SearchBar onSearch={handleSearch} />
+      <div className="reviews-container">
+        {filteredReviews.map((review) => (
+          <ReviewCard key={review.id} review={review} />
+        ))}
+      </div>
+    </div>
     </> 
     
     );
