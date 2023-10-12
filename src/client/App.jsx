c8cb45d57e05d73640f7fef06a5cb5fb85b0c316
CountSamar <trentjohnbrown@gmail.com>
2023-10-12 17:54:21 -0500

    Added serchbar and results

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom'
 import './style.css'
 import Login from './components/Login';
 import Profile from './components/Profile';
@@ -7,7 +7,6 @@ import Review from './components/Review';
 import SearchBar from './components/SearchBar'
 import SignUp from './components/SignUp';
 import NavBar from './components/NavBar';
-import LikeButton from './components/LikeButton';
 import WriteReview from './components/WriteReview';
 import Logout from './components/Logout';
 import Home from './components/Home';
@@ -16,7 +15,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import StarRating from './components/StarRating';
-
+import SearchResults from './components/SearchResults';
 function PrivateRoute({ isLoggedIn }) {
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />;
 }
@@ -45,19 +44,24 @@ export default function App() {
         token={token}
         setShowLogoutMessage={setShowLogoutMessage}
       />
+      <SearchBar />
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<PrivateRoute isLoggedIn={isLoggedIn} />}>
+        <Route
+          path="/profile"
+          element={<PrivateRoute isLoggedIn={isLoggedIn} />}
+        >
           <Route
             index
-            element={<Profile
-             
-              username={username}
-              setToken={setToken}
-              setIsLoggedIn={setIsLoggedIn}
-              setShowLogoutMessage={setShowLogoutMessage}
-            />}
+            element={
+              <Profile
+                username={username}
+                setToken={setToken}
+                setIsLoggedIn={setIsLoggedIn}
+                setShowLogoutMessage={setShowLogoutMessage}
+              />
+            }
           />
         </Route>
         <Route
@@ -74,6 +78,10 @@ export default function App() {
               setToken={setToken}
             />
           }
+        />
+        <Route
+          path="/search-results"
+          element={<SearchResults token={token} />}
         />
         <Route
           path="/login"
