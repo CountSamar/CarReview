4ef69a68e1d817567d9938a2507b7018634b8f23
CountSamar <trentjohnbrown@gmail.com>
2023-10-12 17:56:38 -0500

    Added pagination code

--- src/client/components/Pagination.jsx
+++ src/client/components/Pagination.jsx
@@ -1,23 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import ReviewCard from './ReviewCard';
 
-//Going to make a 5 review list before more load.
-const reviewsPerPage = 3
+const Pagination = () => {
+  const [reviews, setReviews] = useState([...initialReviews]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3; 
+  const totalReviewsCount = 100; 
 
-const Pagination = ({ ReviewCard }) => {
-    const [reviews, setReviews] = useState([...initialReviews])
-    const [currentPage, setCurrentPage] = useState(1)
-    const reviewsPerPage = 3
-
-    const handleScroll = () => {
-        const { }
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      loadMoreReviews();
     }
+  };
 
-    return (
-        <>
-        
-        </>
-     );
-}
- 
+  const loadMoreReviews = () => {
+    const additionalReviews = fetchMoreReviews(currentPage, itemsPerPage);
+    setReviews([...reviews, ...additionalReviews]);
+    setCurrentPage(currentPage + 1);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [currentPage]);  
+
+  return (
+    <div>
+      {/* Display reviews using ReviewCard component */}
+      {reviews.map((review, index) => (
+        <ReviewCard key={index} review={review} />
+      ))}
+    </div>
+  );
+};
+
 export default Pagination;
