ea1ab37fc7d53d39d705ef452aa5d96d32a5b4ed
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-10-20 23:30:07 -0500

    Wizkid

--- src/server/api/server.js
+++ src/server/api/server.js
@@ -2,16 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-
-const PORT = 5001;
-
-
-const userRoutes = require("./routes/userroutes");
-const carRoutes = require("./routes/carroute");
-const reviewRoutes = require("./routes/reviewroute");
-const chatRoutes = require("./routes/chatroute");
-
 const app = express();
+const port = process.env.PORT || 3000;
 
 // List of allowed origins for CORS.
 const allowedOrigins = [
@@ -20,43 +12,47 @@ const allowedOrigins = [
   "https://carreviewweb.onrender.com"
 ];
 
-
-app.use(
-  cors({
-    origin: function(origin, callback) {
-      if (!origin) return callback(null, true);  // Allow requests with no origin like mobile apps or curl requests.
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);  // Allow requests with no origin like mobile apps or curl requests.
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/test", (req, res) => {
-  console.log("Test endpoint hit!");
-  res.json({ message: "This is a test message from the server" });
-});
+// API routes
+const userRoutes = require("./routes/userroutes");
+const carRoutes = require("./routes/carroute");
+const reviewRoutes = require("./routes/reviewroute");
+const chatRoutes = require("./routes/chatroute");
 
 app.use("/api/users", userRoutes);
 app.use("/api/cars", carRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/chats", chatRoutes);
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, '..', '..', 'dist')));
 
+// Route to handle any requests to unhandled endpoints
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html'));
+});
+
+// Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send({ success: false, message: "Something went wrong!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on :${PORT}`);
-});
+// Start the server
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
