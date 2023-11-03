05482836425b9936cc97ce047b7c99e08f558f92
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-11-03 00:29:18 -0500

    1234:

--- src/server/api/multer.js
+++ src/server/api/multer.js
@@ -1,21 +1,23 @@
 const multer = require('multer');
 const path = require('path');
-
-// Ensure the uploads directory exists.
-const uploadDir = path.join(__dirname, '..', 'uploads'); // Adjust the path according to your project structure
 const fs = require('fs');
+
+// Set the directory for the uploads to /CarReview/src/server/uploads
+const uploadDir = path.join(__dirname, '..', 'uploads'); // Goes up one level to /CarReview/src/server
+
+// Ensure the uploads directory exists
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    cb(null, uploadDir); // This will save files in /CarReview/src/server/uploads
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     const ext = path.extname(file.originalname);
-    cb(null, 'imgpath-' + uniqueSuffix + ext); // This line constructs the filename as 'imgpath-timestamp.ext'
+    cb(null, 'imgpath-' + uniqueSuffix + ext); // This saves files as 'imgpath-timestamp.ext'
   }
 });
 
--- src/server/api/server.js
+++ src/server/api/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5001",
-  "https://carreviewweb.onrender.com"
+  "https://carreviewweb.onrender.com",
 ];
 
 // CORS middleware setup.
@@ -51,7 +51,7 @@ const distDir = path.join(__dirname, '..', '..', '..', 'dist');
 console.log(`Serving static files from: ${distDir}`);
 app.use(express.static(distDir));
 
-// Serve static files from the uploads directory
+// Serve static files from the uploads directory in the server directory
 const uploadsDir = path.join(__dirname, 'uploads');
 console.log(`Serving uploads from: ${uploadsDir}`);
 app.use("/uploads", express.static(uploadsDir));
