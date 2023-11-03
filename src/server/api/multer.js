014dea9d090ab2f96cb76b3d66e03694b51f801d
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-11-02 22:46:48 -0500

    update to mutler

--- src/server/api/multer.js
+++ src/server/api/multer.js
@@ -1,14 +1,23 @@
 const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now() + '.' + file.mimetype.split('/')[1]);
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + Date.now() + ext);
   }
 });
 
 const upload = multer({ storage: storage });
 
 module.exports = upload;
+
