bbf9827cd9775d85f319319834df05346db40bfb
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-11-11 22:14:43 -0600

    bucket

--- src/server/api/multer.js
+++ src/server/api/multer.js
@@ -1,27 +1,31 @@
+const AWS = require('aws-sdk');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
 const path = require('path');
-const fs = require('fs');
 
-// Set the directory for the uploads to /CarReview/src/server/uploads
-const uploadDir = path.join( 'uploads');
-// Log the upload directory path
-console.log(`Upload directory is set to: ${uploadDir}`);
-// Ensure the uploads directory exists
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir); // This will save files in /CarReview/src/server/uploads
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const ext = path.extname(file.originalname);
-    cb(null, 'imgpath-' + uniqueSuffix + ext); // This saves files as 'imgpath-timestamp.ext'
-  }
+// Configure the AWS SDK
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Ensure these are set in your environment
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-const upload = multer({ storage: storage });
+const s3 = new AWS.S3();
+
+// Configure multer to use multer-s3 for uploading files to S3
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'reviewbucket333', // Replace with your actual S3 bucket name
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      const ext = path.extname(file.originalname);
+      cb(null, 'imgpath-' + uniqueSuffix + ext); // Naming the file
+    }
+  })
+});
 
 module.exports = upload;
+
