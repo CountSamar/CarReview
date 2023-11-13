d4e0dbb328b34b267a617d4714db8e97d6047d7c
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-11-12 21:25:29 -0600

    redone mutler

--- src/server/api/multer.js
+++ src/server/api/multer.js
@@ -2,39 +2,34 @@ const multer = require('multer');
 const AWS = require('aws-sdk');
 const { Readable } = require('stream');
 
-// Initialize AWS S3 with Access Point ARN
+// Initialize AWS S3
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
- 
-  endpoint: 'arn:aws:s3:us-east-2:812347084154:accesspoint/soulunknown', // Access Point ARN
-  s3ForcePathStyle: true, // Necessary for access points
+  endpoint: 'arn:aws:s3:us-east-2:812347084154:accesspoint/soulunknown', 
+  s3ForcePathStyle: true, 
 });
 
-// Custom multer storage engine
 const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
   fileFilter: function (req, file, cb) {
-    // Add any file filters or validations here
+    // File filter logic
     cb(null, true);
   }
-}).single('imgpath');
+});
 
-// Function to upload a file to S3
 async function uploadFileToS3(file) {
   const params = {
-    Bucket: 'reviewbucket333', // Regular bucket name
-    Key: `imgpath-${Date.now()}-${file.originalname}`, // File key
+    Bucket: 'reviewbucket333',
+    Key: `imgpath-${Date.now()}-${file.originalname}`,
     Body: Readable.from(file.buffer),
-    // Add other necessary S3 parameters like ContentType, ACL, etc.
+    // Additional parameters
   };
 
   return s3.upload(params).promise();
 }
 
-// Export the upload middleware and the S3 upload function
 module.exports = { upload, uploadFileToS3 };
-
 
